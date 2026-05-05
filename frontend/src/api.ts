4244ec77f3cc7967a369cd2edbea472dacb53340
Kunal Singh <kunalindia59@gmail.com>
2026-05-05 23:38:08 +0530

    New functions for api calling

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -15,3 +15,14 @@ export const sigin = () => {
     return api.get('/auth/google')
 }
 
+export const getFullContext = (data: { thread_id: string, user_id: string }) => {
+    return api.post("/mail/fullContext", data)
+}
+
+export const getCompleteThread = (threadId: string) => {
+    return api.get(`/mail/thread/${threadId}`)
+}
+
+export const sendDraftPrompt = async (data: { thread_id: string, user_prompt: string }) => {
+    return api.post("/mail/tweakReply", data)
+}
