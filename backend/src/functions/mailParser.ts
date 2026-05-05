07dfbabc6f1c703f6dbce4b04bb0fc7b1d581057
Kunal Singh <kunalindia59@gmail.com>
2026-05-05 23:37:10 +0530

    Adding new functions for ThreadContext reply generation and for modifying replies or tweaking replies based on user prompt

--- backend/src/functions/mailParser.ts
+++ backend/src/functions/mailParser.ts
@@ -2,7 +2,7 @@ import Anthropic from "@anthropic-ai/sdk";
 import { PARSER_PROMPT } from "../prompts/parserPrompt.js";
 import type { TextBlock } from "@anthropic-ai/sdk/resources";
 import dotenv from "dotenv"
-import { ContextPrompt, ReplyPrompt } from "../prompts/basePrompt.js";
+import { ContextBasedReplyPrompt, ContextPrompt, ReplyPrompt, UserBasedReplyPrompt } from "../prompts/basePrompt.js";
 dotenv.config()
 type Mail = {
     from: string // email of sender
@@ -23,7 +23,9 @@ const client = new Anthropic({
     apiKey: CLAUDE_API_KEY
 })
 
-export const parseMail = async (body: string) => {
+export const parseMail = async (body: string = "No Body") => {
+    // console.log('Body is : ', body)
+    // return
     const response = await client.messages.create({
         max_tokens: 10000,
         model: 'claude-sonnet-4-6',
@@ -71,6 +73,47 @@ export const getContext = async (content_array: { body: string, html: string, fr
             {
                 role: "user",
                 content: `${content_array}`
+            }
+        ]
+    })
+    // console.log(response.content[0])
+    const answer: string = (response.content[0] as TextBlock).text
+    return answer
+}
+
+export const generateReplyBasedOnThreadContext = async (context: string, mails: { body: string, html: string, intent: string }[], email: string) => {
+    const SYSTEM_PROMPT = ContextBasedReplyPrompt(mails, email, context)
+    const response = await client.messages.create({
+        max_tokens: 15000,
+        model: 'claude-sonnet-4-6',
+        system: SYSTEM_PROMPT,
+        temperature: 1,
+        messages: [
+            {
+                role: "user",
+                content: `Mails : ${JSON.stringify(mails)} , Thread Context : ${context}`
+            }
+        ]
+    })
+    // console.log(response.content[0])
+    const answer: string = (response.content[0] as TextBlock).text
+    return answer
+}
+export const generateReplyBasedOnThreadContextAndUserPrompt = async (context: string, mails: { body: string, html: string, intent: string }[], email: string, user_prompt: string) => {
+    const SYSTEM_PROMPT = UserBasedReplyPrompt(mails, email, context, user_prompt)
+    const response = await client.messages.create({
+        max_tokens: 15000,
+        model: 'claude-sonnet-4-6',
+        system: SYSTEM_PROMPT,
+        temperature: 1,
+        messages: [
+            {
+                role: "user",
+                content: `Mails : ${JSON.stringify(mails)} , Thread Context : ${context}`
+            },
+            {
+                role: "user",
+                content: `I have provided you mails and thread context and here is my requirement : ${user_prompt}`
             }
         ]
     })
